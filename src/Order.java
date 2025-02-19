0d91818687e2c27163eed26fc29c3563e47af6fa
bbqstand <mashrurfaiyaz@iut-dhaka.edu>
2025-02-19 15:41:47 +0600

    defined the Order class properly

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Flavor> flavors = new ArrayList<>();
+    private List<Topping> toppings = new ArrayList<>();
+    private Container container;
+
+    public void addFlavor(Flavor flavor){
+        flavors.add(flavor);
+    }
+
+    public void removeFlavor(Flavor flavor){
+        flavors.remove(flavor);
+    }
+
+    public void addTopping(Topping topping){
+        toppings.add(topping);
+    }
+
+    public void removeTopping(Topping topping){
+        toppings.remove(topping);
+    }
+
+    public void setContainer(Container container){
+        this.container = container;
+    }
+
+    public List<Flavor> getFlavors(){
+        return flavors;
+    }
+
+    public List<Topping> getToppings(){
+        return toppings;
+    }
+
+    public Container getContainer(){
+        return container;
+    }
 }
