1530b1f85c148d2b60d542d51a0549222ed47f37
bbqstand <mashrurfaiyaz@iut-dhaka.edu>
2025-02-19 15:25:54 +0600

    first

---
+++ src/Topping.java
@@ -0,0 +1,9 @@
+public class Topping {
+    private String name;
+    private double price;
+
+    public Topping(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+}
